d96ed4ca39ad0103fc9d30b995908bf807c1b613
Hayyan67 <humairabaig81@gmail.com>
2024-10-19 14:58:43 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+a={2,5,7,3,23,65,42,2,3}
+b={12,3,5,7,2,55,34,65}
+print('union: ',a.union(b))
+print('intersection: ',a.intersection(b))
+print('difference: ',a.difference(b))
+print('Symmetric difference: ',a.symmetric_difference(b))
